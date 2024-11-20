fc263b59eaffeed8eb733a0936788fbae7b52b06
Umakant-Mahajan <umakantmahajan9@gmail.com>
2024-11-20 15:35:20 -0500

    Models and makes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'umakant22110-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'umakant22110-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://umakant22110-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://umakant22110-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
